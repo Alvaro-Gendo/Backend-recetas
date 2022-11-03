6836102f30e93fe8310bba6f57e3d3c447fd5ca2
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-11-02 21:48:40 -0300

    agregue coneccion con BD y cree modelo de usuarios y receta

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import morgan from "morgan";
 import cors from "cors";
 import path from "path";
 import rutas from "./routes/receta.routes";
-
+import "./database";
 const app = express();
 
-app.set("port", process.env.PORT || 4000)
+app.set("port", process.env.PORT || 4000);
 
-app.listen(app.get("port"), ()=>{
-    console.log("Puerto " + app.get("port"))
-})
+app.listen(app.get("port"), () => {
+  console.log("Puerto " + app.get("port"));
+});
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/apireceta", rutas)
+app.use("/apireceta", rutas);
